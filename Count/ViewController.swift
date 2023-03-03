9a21a5663d5bc718a100ef8b1a9078243a9be2f9
marijin <marijirus@gmail.com>
2023-03-03 16:00:04 +0300

    Добавлена функция подсчета

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countOfButton: UIButton!
+    
+    var count: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func countButtonDidTap(_ sender: Any) {
+        count += 1
+        countLabel.text = String(count)
+    }
+    
 
 }
 
